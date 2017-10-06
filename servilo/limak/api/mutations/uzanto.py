902f50e5b090860f87945b08d1370f9ae5a8887a
JCharante <git@jcharante.com>
2017-10-06 10:26:42 -0400

    ripari refactor eraro

--- servilo/limak/api/mutations/uzanto.py
+++ servilo/limak/api/mutations/uzanto.py
@@ -36,7 +36,7 @@ class KreiKunsidonKunGoogle(graphene.Mutation):
 
 		kunsidon = Kunsidon(kunsidon_id=kunsidon_id, valida_gxis=valida_gxis)
 
-		return KreiKunsidon(kunsidon=kunsidon)
+		return KreiKunsidonKunGoogle(kunsidon=kunsidon)
 
 
 class KreiKunsidonKunRetposxto(graphene.Mutation):
@@ -59,7 +59,7 @@ class KreiKunsidonKunRetposxto(graphene.Mutation):
 
 		kunsidon = Kunsidon(kunsidon_id=kunsidon_id, valida_gxis=valida_gxis)
 
-		return KreiKunsidon(kunsidon=kunsidon)
+		return KreiKunsidonKunRetposxto(kunsidon=kunsidon)
 
 
 class KreiKontoKunRetposxto(graphene.Mutation):
@@ -85,37 +85,4 @@ class KreiKontoKunRetposxto(graphene.Mutation):
 
 		kunsidon = Kunsidon(kunsidon_id=kunsidon_id, valida_gxis=valida_gxis)
 
-		return KreiKunsidon(kunsidon=kunsidon)
-
-
-class KreiKunsidon(graphene.Mutation):
-	class Input:
-		aliro_token = graphene.NonNull(graphene.ID)
-
-	kunsidon = graphene.Field(Kunsidon)
-
-	@staticmethod
-	def mutate(root, args, context, info):
-		kreditoj = google.oauth2.credentials.Credentials(args.get('aliro_token'))
-		rajtigita_kunsido = AuthorizedSession(kreditoj)
-		respondo = rajtigita_kunsido.get('https://www.googleapis.com/userinfo/v2/me')
-		respondo = respondo.json()
-
-		google_id = respondo.get('id')
-		try:
-			uzanto_id = db_funkcioj.akiri_uzanton_id(google_id)
-		except esceptoj.GoogleIdNeTrovita:
-			uzanto_id = db_funkcioj.krei_konton(respondo.get('email'),
-												google_id,
-												respondo.get('given_name'),
-												respondo.get('family_name'),
-												respondo.get('name'),
-												respondo.get('picture'),
-												respondo.get('hd', 'gmail.com')
-												)
-
-		valida_gxis, kunsidon_id = db_funkcioj.krei_kunsidon(uzanto_id)
-
-		kunsidon = Kunsidon(kunsidon_id=kunsidon_id, valida_gxis=valida_gxis)
-
-		return KreiKunsidon(kunsidon=kunsidon)
+		return KreiKontoKunRetposxto(kunsidon=kunsidon)
