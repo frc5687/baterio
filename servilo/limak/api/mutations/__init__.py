a102556a2b5516d57283be375da933aa2d249916
John <git@jcharante.com>
2017-10-15 20:20:42 -0400

    Aldonu MutateBaterio

--- servilo/limak/api/mutations/__init__.py
+++ servilo/limak/api/mutations/__init__.py
@@ -1,12 +1,21 @@
 import graphene
-from limak.api.mutations.uzanto import KreiKunsidonKunGoogle, KreiKontoKunRetposxto, KreiKunsidonKunRetposxto
+from limak.api.mutations import uzanto, baterio
+from limak.db import funkcioj
+from limak.api import esceptoj
 
 
 class Mutation(graphene.ObjectType):
 	motd = graphene.NonNull(graphene.String)
-	krei_kunsidon_kun_google = KreiKunsidonKunGoogle.Field()
-	krei_kunsidon_kun_retposxto = KreiKunsidonKunRetposxto.Field()
-	krei_konto_kun_retposxto = KreiKontoKunRetposxto.Field()
+	krei_kunsidon_kun_google = uzanto.KreiKunsidonKunGoogle.Field()
+	krei_kunsidon_kun_retposxto = uzanto.KreiKunsidonKunRetposxto.Field()
+	krei_konto_kun_retposxto = uzanto.KreiKontoKunRetposxto.Field()
+	baterio = graphene.Field(baterio.MutateBaterio, kunsidon_id=graphene.NonNull(graphene.ID))
 
 	def resolve_motd(self, args, context, info):
 		return 'Saluton'
+
+	def resolve_baterio(self, args, context, info):
+		if funkcioj.estas_valida_kunsido_id(args.get('kunsidon_id')):
+			return baterio.MutateBaterio()
+		else:
+			raise esceptoj.GeneraEraro('Nevalida kunsido id')
--- servilo/limak/api/mutations/baterio.py
+++ servilo/limak/api/mutations/baterio.py
@@ -1,9 +1,33 @@
 import graphene
-from limak.api.types import Kunsidon
+from limak.api import types, esceptoj
 from limak.db import funkcioj as db_funkcioj
-from limak.api import esceptoj
 
 
 class RegistriBaterio(graphene.Mutation):
+	"""
+	Registri nova baterio. Neniu volos registri nova baterio ĉe konkurenco, do
+	ĉi tio ne subteno senkonekta baterio id generacio.
+	"""
 	class Input:
-		pass
+		baterio_nomo = graphene.NonNull(graphene.String)
+		modelo = graphene.NonNull(graphene.String)
+
+	baterio = graphene.Field(types.Baterio)
+
+	@staticmethod
+	def mutate(root, args, context, info):
+		baterio_id = db_funkcioj.krei_baterio(
+			baterio_nomo=args.get('baterio_nomo'),
+			modelo=args.get('modelo')
+		)
+		return RegistriBaterio(
+			baterio=types.Baterio(
+				baterio_id=baterio_id,
+				baterio_nomo=args.get('baterio_nomo'),
+				modelo=args.get('modelo')
+			)
+		)
+
+
+class MutateBaterio(graphene.ObjectType):
+	registri_baterio = RegistriBaterio.Field()
